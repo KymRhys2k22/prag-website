6789d071158ce77330814e5abbdbf88d8aa39397
KymRhys2k22 <kymrhys@gmail.com>
2023-06-20 15:49:46 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import prag from "./assets/prag.jpg"
+
+const App = () => {
+  return (
+    <div className='w-auto h-screen flex items-center justify-center'>
+      <a href='https://www.facebook.com/p/Peace-Reaction-Assistance-Group-Prag-100082141553393/' target='_blank'><img src={prag} alt="" height={300} width={300} className='animate-pulse '/></a >
+    </div>
+  )
+}
+
+export default App
