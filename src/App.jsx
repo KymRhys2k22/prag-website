c2fc2df4f872da0254da47ca4041d5c79747f1f8
KymRhys2k22 <kymrhys@gmail.com>
2023-06-20 16:28:45 +0800

    add animation and hover

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import prag from "./assets/prag.jpg"
 const App = () => {
   return (
     <div className='w-auto h-screen flex items-center justify-center'>
-      <a href='https://www.facebook.com/p/Peace-Reaction-Assistance-Group-Prag-100082141553393/' target='_blank'><img src={prag} alt="" height={300} width={300} className='animate-pulse '/></a >
+      <a href='https://www.facebook.com/p/Peace-Reaction-Assistance-Group-Prag-100082141553393/' target='_blank'><img src={prag} alt="" height={300} width={300} className='animate-pulse hover:scale-95 transition-all '/></a >
     </div>
   )
 }
